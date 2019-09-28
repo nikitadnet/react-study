17ce5258c8775ee7262581c62e79a8191579358b
nikita <demchenkonikitko@gmail.com>
2019-09-28 12:57:37 +0300

    JSX

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Car from './Car/Car'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    cars: [
+      {name: 'Ford', year: 2018},
+      {name: 'Audi', year: 2016},
+      {name: 'Mazda', year: 2010},
+    ],
+    pageTitle: 'React components'
+  }
+
+  render() {
+    const divStyle = {
+      textAlign: 'center',
+    }
+
+    const cars = this.state.cars;
+
+    return (
+      <div style={divStyle}>
+        <h1>{this.state.pageTitle}</h1>
+
+        <Car name={cars[0].name} year={cars[0].year}></Car>
+        <Car name={cars[1].name} year={cars[1].year}></Car>
+        <Car name={cars[2].name} year={cars[2].year}></Car>
+      </div>
+    );
+  }
 }
+
 
 export default App;
