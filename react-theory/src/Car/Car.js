9b2f7c69ae70efb0cf4cdcbbe6864b737a53950f
nikita <demchenkonikitko@gmail.com>
2019-10-16 16:35:04 +0300

    Жизненный цикл удаления

--- react-theory/src/Car/Car.js
+++ react-theory/src/Car/Car.js
@@ -25,6 +25,10 @@ class Car extends React.Component {
         console.log('Car componentDidUpdate');
     }
 
+    componentWillUnmount() {
+        console.log('Car componentWillUnmount')
+    }
+
     render() {
         console.log('Car Render')
         const inputClasses = ['input'];
